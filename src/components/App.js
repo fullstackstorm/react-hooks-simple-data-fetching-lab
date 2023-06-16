3fc2c5d5922506c0b7a1e78732ff6783981d2342
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-06-16 13:30:34 -0600

    Who let the dogs out?

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [randomDog, setDog] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not OK");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setDog(data.message);
+        setIsLoaded(true);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the request
+        console.error("Error:", error);
+      });
+  }, []);
+
+  if(!isLoaded) return <p>Loading...</p>;
+
+  return (<img src={randomDog} alt="A Random Dog"/>)
+}
+
+export default App;
